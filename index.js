4210d72bc56ba1cff14d578dbfe32a2d346f74d3
tungngu345 <davistighepun0suy@hotmail.com>
2024-05-13 18:22:59 +0700

    update sdk project commit

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-const radixSort = (arr) => {
-  const getDigit = (num, place) =>
-    Math.floor(Math.abs(num) / Math.pow(10, place)) % 10;
-  const digitCount = (num) =>
-    num === 0 ? 1 : Math.floor(Math.log10(Math.abs(num))) + 1;
-  const mostDigits = (arr) => Math.max(...arr.map((num) => digitCount(num)));
-  const maxDigits = mostDigits(arr);
-  for (let k = 0; k < maxDigits; k++) {
-    let digitBuckets = Array.from({ length: 10 }, () => []);
-    for (let i = 0; i < arr.length; i++) {
-      digitBuckets[getDigit(arr[i], k)].push(arr[i]);
-    }
-    arr = [].concat(...digitBuckets);
-  }
-  return arr;
-};
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
